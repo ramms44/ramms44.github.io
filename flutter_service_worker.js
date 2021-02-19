7bdb9dbe32cf01a6712d43ff3ad7f66a04db9017
ramms11 <madeapps11@gmail.com>
2021-02-20 02:47:05 +0700

    update pagesss #22

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,8 +21,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "dc9bc44b2e8499e8415ca5eea97c460a",
-"/": "dc9bc44b2e8499e8415ca5eea97c460a",
+"index.html": "d8c3c77dda4a566fa0294bb71454f405",
+"/": "d8c3c77dda4a566fa0294bb71454f405",
 "main.dart.js": "3ac1bfcb7dcb72b01bb4f0c8d0af0094",
 "manifest.json": "5980d2e9a37951fb41cfa1e767fc1d41",
 "version.json": "4d16d37eae96bba8bdeee7cb6000da1d"
