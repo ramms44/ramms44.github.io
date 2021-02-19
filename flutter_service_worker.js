bee181f46b17db7078b32296daf3528007edca5b
ramms11 <madeapps11@gmail.com>
2021-02-19 17:17:23 +0700

    update github pFWA #3

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -21,9 +21,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "03a60f789bce408b9fc482069708b795",
-"/": "03a60f789bce408b9fc482069708b795",
-"main.dart.js": "ef88eb8a08262521a85a2cd92f1fad57",
+"index.html": "e5225eb78fb4496d55194e03ef9072e1",
+"/": "e5225eb78fb4496d55194e03ef9072e1",
+"main.dart.js": "21eb971ce9363fdf0c189ca114255548",
 "manifest.json": "5980d2e9a37951fb41cfa1e767fc1d41",
 "version.json": "4d16d37eae96bba8bdeee7cb6000da1d"
 };
